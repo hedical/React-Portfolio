2052d449ce1b6e72835d8581a949517f6c02453e
Hedi Calabrese <calabrese.hedi@gmail.com>
2020-08-28 14:48:32 -0700

    Updates

---
+++ precache-manifest.088c2ee3546181dbea041d0a7d663b1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "042991d414a0a8ebccbe94d625958726",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "d41a3d547506e2f457ee",
+    "url": "/React-Portfolio/static/css/main.c2de7e9e.chunk.css"
+  },
+  {
+    "revision": "3a10c3f3e985e627315d",
+    "url": "/React-Portfolio/static/js/2.5886da71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Portfolio/static/js/2.5886da71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d41a3d547506e2f457ee",
+    "url": "/React-Portfolio/static/js/main.73d42476.chunk.js"
+  },
+  {
+    "revision": "7c35ff808cd94c4ce20c",
+    "url": "/React-Portfolio/static/js/runtime-main.0ad08de7.js"
+  }
+]);
